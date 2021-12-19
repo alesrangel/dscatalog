1ec4c03d0d4043b7e4eb2f8698e6b5853f131e6b
alesrangel <alesrangel15@gmail.com>
2021-12-19 15:35:03 -0300

    tratamento de excecao database

--- dscatalog/backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ dscatalog/backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -7,12 +7,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
+import com.devsuperior.dscatalog.services.exceptions.DatabaseException;
 import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -20,11 +23,10 @@ public class CategoryService {
 
 	@Autowired
 	private CategoryRepository repository;
-	
-	
+
 	@Transactional(readOnly = true)
-	public List<CategoryDTO> findAll(){
-		List<Category> list =  repository.findAll();
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
 		return list.stream().map(category -> new CategoryDTO(category)).collect(Collectors.toList());
 	}
 
@@ -42,7 +44,7 @@ public class CategoryService {
 		entity = repository.save(entity);
 		return new CategoryDTO(entity);
 	}
- 
+
 	@Transactional
 	public CategoryDTO update(Long id, CategoryDTO dto) {
 		try {
@@ -52,6 +54,16 @@ public class CategoryService {
 			return new CategoryDTO(entity);
 		} catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id Not Found " + id);
+		}
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		} catch(DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
 		}
 	}
 }
